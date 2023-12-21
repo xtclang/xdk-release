fd36e0b24be9f6a83472ef0e59e46221c6684ce7
Marcus Lagergren <1062473+lagergren@users.noreply.github.com>
2023-12-21 19:58:51 +0100

    Updated scripts

--- scripts/check-github-token-privileges.sh
+++ scripts/check-github-token-privileges.sh
@@ -1,2 +1,15 @@
-curl -sS -f -I -H "Authorization: token $TOKEN" https://api.github.com | grep -i x-oauth-scopes
+#!/bin/sh
 
+# Helper script that prints the privileges associated with a GitHub token.
+# This can be useful if you want to expose a token to a non-private group, so that
+# it can be programmatically or manually double checked whether it only had the
+# intended privileges. For example: no write privileges, and read:package only.
+
+function check_github_token() {
+    token=$1
+    if [ -z $token ]; then
+	echo "Missing argument: GitHub token to inspect."
+    else
+	curl -sS -f -I -H "Authorization: token $token" https://api.github.com | grep -i x-oauth-scopes
+    fi
+}
